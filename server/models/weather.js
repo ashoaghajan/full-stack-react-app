048ef0a7e5216f06abe236b17e21b8d08588dd2c
Ashot <shant@debian.Home>
2020-03-18 18:54:12 +0400

    API key fixed

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,13 +1,11 @@
 const request = require('request-promise');
 
-require('dotenv').config();
-
 const API_KEY = '56e3112f41a7c9bdd81ad6fc5eed1bf3'
 
 class Weather {
   static retrieveByCity (city, callback) {
     request({
-      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=imperial`,
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
       json: true
     }).then((res) => {
       callback(res);
